7f62b89830d65e8451300772dce19a23de02b073
modkhalid <modkhalid5@gmail.com>
2019-01-03 12:42:43 +0530

    insertion sort -java

---
+++ java/src/Algorithms/InserionSort.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+import java.util.Scanner;
+
+public class InserionSort {
+	public static void sort_insertion(int arr[]) {
+		for (int i = 1; i < arr.length; i++) {
+			int key=arr[i];
+			int j=i-1;
+			while(j>=0 && arr[j]>key){
+				arr[j+1]=arr[j];
+				j--;
+			}
+			arr[j+1]=key;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int arr[]=new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=sc.nextInt();
+		}
+		sort_insertion(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+}
