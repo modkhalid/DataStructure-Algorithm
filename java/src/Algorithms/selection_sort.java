1003295616dfccba03e1153e0596bf7628b2c7c6
modkhalid <modkhalid5@gmail.com>
2019-01-03 12:22:38 +0530

    selection sort -java

--- java/src/Algorithms/selection_sort.java
+++ java/src/Algorithms/selection_sort.java
@@ -15,6 +15,22 @@ public class selection_sort {
 			}
 		}
 	}
+	
+	public static void sort_best(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			int min_i=i;
+			for (int j = i+1; j < arr.length; j++) {
+				if(arr[j]<arr[min_i]) {
+					min_i=j;
+				}
+			}
+			if(i!=min_i) {
+				int temp=arr[i];
+				arr[i]=arr[min_i];
+				arr[min_i]=temp;
+			}
+		}
+	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int N=sc.nextInt();
@@ -22,7 +38,8 @@ public class selection_sort {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i]=sc.nextInt();
 		}
-		sort(arr);
+//		sort(arr);
+		sort_best(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]+" ");
 		}
