7871e35501c226a8b0065d59e9fe1434b3ede73d
modkhalid <modkhalid5@gmail.com>
2019-01-03 12:05:52 +0530

    selection sort

---
+++ java/src/Algorithms/selection_sort.java
@@ -0,0 +1,31 @@
+package Algorithms;
+
+import java.util.Scanner;
+
+public class selection_sort {
+	
+	public static void sort(int arr[]) {
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if(arr[i]>arr[j]) {
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int arr[]=new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=sc.nextInt();
+		}
+		sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+}
