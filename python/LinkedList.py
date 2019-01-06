fb8595322637febd85d2b9a711dfc7fa36a2a62b
modkhalid <modkhalid5@gmail.com>
2019-01-06 23:32:41 +0530

    LinkedLIst reversePI-python

--- python/LinkedList.py
+++ python/LinkedList.py
@@ -1,87 +1,3 @@
-# class Node:
-#     def __init__(self,item):
-#         self.data=item
-#         self.next=None
-#
-# class LinkedList:
-#     def __init__(self):
-#         self.head=None
-#         self.tail=None
-#         self.size=0
-#
-#     def addFirst(self,item):
-#         nn=Node(item)
-#         if self.head is None:
-#             self.head=nn
-#             self.tail=nn
-#         else:
-#             nn.next=self.head
-#             self.head=nn
-#         self.size+=1
-#
-#     def addLast(self,item):
-#         nn=Node(item)
-#         if self.head is None:
-#             self.head=nn
-#             self.tail=nn
-#         else:
-#             self.tail.next=nn
-#             self.tail=nn
-#
-#         self.size+=1
-#     def getNodeAt(self,idx):
-#         if idx >= self.size and idx <0:
-#             print("Invalid Index")
-#             return
-#         if idx is 0:
-#             return self.head
-#         elif idx is self.size-1:
-#             return self.tail
-#         else:
-#             temp=self.head
-#             for i in range(0,idx):
-#                 temp=temp.next
-#             return temp
-#     def addAt(self,idx,item):
-#         if idx >= self.size and idx <0:
-#             print("Invalid Index")
-#             return
-#         if idx==0:
-#             return self.addFirst(item)
-#         elif idx is self.size:
-#             return self.addLast(item)
-#         else:
-#             n=self.getNodeAt(idx-1)
-#             nn=Node(item)
-#             nn.next=n.next
-#             n.next=nn
-#             self.size+=1
-#
-#
-#
-#
-#     def display(self):
-#         temp=self.head
-#         while temp is not None:
-#             print(temp.data,end=" ")
-#             temp=temp.next
-#
-#
-#
-# ll=LinkedList()
-#
-# ll.addFirst(9)
-# ll.addFirst(8)
-# ll.addFirst(7)
-# ll.addFirst(6)
-# ll.addFirst(5)
-# ll.addAt(3,6.5)
-# print(ll.getNodeAt(4).data)
-#
-# ll.display()
-
-
-
 class Node:
     def __init__(self,data):
         self.data=data
@@ -176,7 +92,24 @@ class LinkedList:
             return data
 
 
+    def reverseDI(self):
+        for i in range(0,int(self.size/2)):
+            t1=self.getNodeAt(i).data
+            self.getNodeAt(i).data=self.getNodeAt(self.size-1-i).data
+            self.getNodeAt(self.size-1-i).data=t1
 
+    def reversePI(self):
+        prev=None
+        temp=None
+        curr=self.head
+        while curr is not None:
+            temp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=temp
+        temp=self.tail
+        self.tail=self.head
+        self.head=temp;
 
 
 
@@ -198,7 +131,10 @@ ll.addFirst(2)
 ll.addAt(2,4)
 ll.display()
 print('\n---------')
-print(ll.removeAt(2))
+# print(ll.removeAt(2))
+ll.reversePI()
+ll.reversePI()
 ll.display()
 
 print('\n---------\n')
+# ll.x();
