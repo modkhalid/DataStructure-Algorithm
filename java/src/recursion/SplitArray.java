d5ccffccefbba022e76bb99fc65228435e8722f8
modkhalid <modkhalid5@gmail.com>
2019-02-12 16:38:34 +0530

    split array count for sum

--- java/src/recursion/SplitArray.java
+++ java/src/recursion/SplitArray.java
@@ -14,9 +14,26 @@ public class SplitArray {
 		split(arr,vidx+1,s1,s2+res,ans1,ans2+" "+res);
 		
 	}
+	
+	public static int splitCount(int arr[],int vidx,int s1,int s2) {
+		if(vidx==arr.length) {
+			int res=0;
+			if(s1==s2) {
+				res=1;				
+			}
+			return res;
+		}
+		int ans=arr[vidx];
+		int res=0;
+		res+=splitCount(arr, vidx+1, s1+ans, s2);
+		res+=splitCount(arr, vidx+1, s1, s2+ans);
+		return res;
+		
+	}
 	public static void main(String[] args) {
 		int arr[]= {1,2,3};
-		split(arr, 0, 0, 0, "", "");
+//		split(arr, 0, 0, 0, "", "");
+		System.out.println(splitCount(arr, 0, 0, 0));
 		
 	}
 }
