2d594061490bbb3a3176d0e0356eed0cb33c3371
modkhalid <modkhalid5@gmail.com>
2019-04-01 09:26:41 +0530

    add  space first  pattern

--- java/src/L2_basic1/Pattern.java
+++ java/src/L2_basic1/Pattern.java
@@ -24,19 +24,35 @@ class PatternDeveloper{
 	
 	
 	public void l2() {
-		for(int i=0;i<this.n;i++) {
+		for(int i=0;i<n;i++) {
 			for(int j=i;j<this.n;j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
+	
+	public void l3() {
+		int counter=n-1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if(counter>j) {
+					System.out.print(" ");
+				}else
+					System.out.print("*");
+			}
+			System.out.println();
+			counter--;
+		}
+	}
+	
+	
 }
 
 
 public class Pattern {
 	public static void main(String[] args) {
 		PatternDeveloper pd=new PatternDeveloper();
-		pd.l2();
+		pd.l3();
 	}
 }
