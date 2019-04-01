79db9f1415889e37f7c8bae528e0072300fc9914
modkhalid <modkhalid5@gmail.com>
2019-04-01 09:31:36 +0530

    square frame

--- java/src/L2_basic1/Pattern.java
+++ java/src/L2_basic1/Pattern.java
@@ -60,6 +60,18 @@ class PatternDeveloper{
 		}
 	}
 	
+	public void squareFrame() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if(i==0 || j==0 || i==n-1 || j==n-1)
+					System.out.print("*");
+				else 
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	
 	
 }
 
@@ -67,6 +79,6 @@ class PatternDeveloper{
 public class Pattern {
 	public static void main(String[] args) {
 		PatternDeveloper pd=new PatternDeveloper();
-		pd.l4();
+		pd.squareFrame();
 	}
 }
